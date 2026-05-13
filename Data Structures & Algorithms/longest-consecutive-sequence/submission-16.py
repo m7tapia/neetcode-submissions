be0b24d8fbaa541cfd172db8a326e36f62fb3736
Mario Tapia <m7tapia@ucsd.edu>
2026-05-13 13:39:06 -0700

    Add: longest-consecutive-sequence - submission-16

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-16.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        #the optimal solution is to use convert the nums into a set
+        #now the tricky part is finding the start of a sequence
+        #if our number is 100 and is the start of the sequence, then it won't have a left neighbor 99 in the set.
+        #That's how we identify the start of a sequence.
+
+        uniq_nums = set(nums)
+
+        highest_count = 1
+        #go through each unique number
+        for num in uniq_nums:
+            count = 1
+            #if its the start of a sequence
+            if num - 1 not in uniq_nums:
+                next = num + 1
+                #keep on checking if there's the next number in the set
+                while next in uniq_nums:
+                    count += 1
+                    next += 1
+            #if count is higher than our highest, than update highest
+            if count > highest_count:
+                highest_count = count
+                
+        return highest_count if nums else 0
+
+
