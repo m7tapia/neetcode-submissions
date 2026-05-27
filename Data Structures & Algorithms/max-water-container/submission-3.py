20fa3e48ad572e11896cb150dd2a491ec97be126
Mario Tapia <m7tapia@ucsd.edu>
2026-05-27 09:41:00 -0700

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        max_area = 0
+
+        for i in range(len(heights)):
+            i_height = heights[i]
+            #starting one ahead of i b/c if we start from the beginning for j, we will be redoing combinations we've already done
+            for j in range(i + 1, len(heights)):
+                j_height = heights[j]
+                min_height = min(i_height, j_height)
+                area = min_height * (j - i)
+                if area > max_area:
+                    max_area = area
+
+        return max_area
+
+
+        #this is the brute force solution. checking every single combination to see which one gives us
+        #the most area
+        #O(n^2) time complexity and O(1) space complexity
+
+
+        #for this problem the area of each combination is the height of the shorter bar * the space between both bars.
