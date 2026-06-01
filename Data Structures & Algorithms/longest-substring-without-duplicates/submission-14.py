f76d7dcbe3b0e4e6595159b8842dcb26987b0976
Mario Tapia <m7tapia@ucsd.edu>
2026-05-31 18:13:41 -0700

    Add: longest-substring-without-duplicates - submission-14

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-14.py
@@ -0,0 +1,30 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l, r = 0, 0
+        longest = 0
+        hash_set = set()
+        curr_length = 0
+        
+        while r < len(s):
+            prev_size = len(hash_set)
+            hash_set.add(s[r])
+            #see if a dupe was added
+            if prev_size == len(hash_set):
+                hash_set.remove(s[l])
+                l += 1
+                #if a dupe (s[r]) was found, the loop will keep on removing the left pointer until it finds where
+                #the other s[r] and then remove both. Then we add s[r] again starting at this new unique substring.
+                
+            else: #if a dupe wasn't found keep on moving right pointer forward
+                r += 1
+                #we only have to keep track of our size here b/c in we're in the if statement the size of our window
+                #is always decreasing and we want the max window
+                curr_length = len(hash_set)
+                longest = max(longest, curr_length)
+                
+        return longest
+
+
+        #This way is using the set and checking its length instead of using the in operator
+
+        #O(n) time and and O(n) space
