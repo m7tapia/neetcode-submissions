aa8a34a7ad905d882eece0c22f8dcc6f8ca6c4eb
Mario Tapia <m7tapia@ucsd.edu>
2026-06-30 23:51:44 -0700

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.py
@@ -0,0 +1,29 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        #sort the arrays to have it like a number line with their positions
+        #figure out at what time t each car will reach target doing (dest - pos) / speed
+        #start at right b/c if a car from the left reaches the right, then they become a fleet and we remove the car on the left and keep the rights speed
+
+        #pair together so they stay with pair when sorting positions
+        pairs = [(position[i], speed[i]) for i in range(len(position))]
+        pairs.sort() #sorts least to greatest
+        #list comprehension to get how long it takes each car to get to target
+        times = [(target - pairs[i][0]) / pairs[i][1] for i in range(len(position))]
+
+        curr_time = 0
+        count = 0
+        for i in range(len(times) - 1, -1, -1):
+            if times[i] > curr_time:
+                count +=1
+                curr_time = times[i]
+
+        return count
+
+        
+
+
+
+
+
+
+        
